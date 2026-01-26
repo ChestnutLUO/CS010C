711f32f89019b14a79aea39a4c8253c6fca41073
ChestnutLUO <digby@65mail.cn>
2026-01-25 22:07:43 -0800

    cpplint fix format

--- list_doubly_linked.h
+++ list_doubly_linked.h
@@ -45,18 +45,18 @@ class ListDoublyLinked {
         // TODO: Implement helper method to get node at position
         // Q 1.3 Now implement private method GetNode() which returns a pointer to a node in the list at a given position.
         // Return pointer on node located as position @pos
-        Node* GetNode(size_t pos){
+        Node* GetNode(size_t pos) {
             assert(pos < cur_size);
-            if(pos>=(cur_size/2)){
+            if (pos >= (cur_size/2)) {
                 Node* n = tail;
                 pos = cur_size - pos - 1;
                 while (pos--) {
                     n = n->previous;
                 }
                 return n;
-            }else{
+            } else {
                 Node* n = head.get();
-                while (pos--){
+                while (pos--) {
                     n = n->next.get();
                 }
                 return n;
@@ -65,16 +65,16 @@ class ListDoublyLinked {
 
         Node* GetNode(size_t pos) const{
             assert(pos < cur_size);
-            if(pos>=(cur_size/2)){
+            if (pos >= (cur_size/2)) {
                 Node* n = tail;
                 pos = cur_size - pos - 1;
                 while (pos--) {
                     n = n->previous;
                 }
                 return n;
-            }else{
+            } else {
                 Node* n = head.get();
-                while (pos--){
+                while (pos--) {
                     n = n->next.get();
                 }
                 return n;
@@ -89,13 +89,13 @@ class ListDoublyLinked {
     ~ListDoublyLinked() = default;
     // TODO: Q 2.2 Implement the Size() method.
     // Return number of items in list
-    size_t Size() const{
+    size_t Size() const {
         return cur_size;
     }
 
     // TODO: Q 2.3 Implement the Get() method.
     // Return item at position @pos
-    const T& Get(const size_t pos) const{
+    const T& Get(const size_t pos) const {
          if (pos >= cur_size) {
             throw std::out_of_range("Position out of range!");
         }
@@ -105,11 +105,11 @@ class ListDoublyLinked {
 
     // TODO: Q 2.4 Implement the Find method.
     // Return position of first occurrence of @item (-1 if not found)
-    int Find(const T &item){
+    int Find(const T &item) {
         Node* current = head.get();
         int pos = 0;
-        for(;current;current=current->next.get(),pos++){
-            if(current->item==item){
+        for (; current; current = current->next.get(), pos++) {
+            if (current->item == item) {
                 return pos;
             }
         }
@@ -119,25 +119,25 @@ class ListDoublyLinked {
     // TODO: Q 2.5 Discuss the different scenarios for item removal.
     // Using these possible combinations, implement the Remove method.
     // Remove item at position @pos
-    void Remove(const unsigned int pos){
-        if(!head || pos >= cur_size){
+    void Remove(const unsigned int pos) {
+        if (!head || pos >= cur_size) {
             return;
-        }else if(pos == 0){
+        } else if (pos == 0) {
             head = std::move(head->next);
-            if(!head){
+            if (!head) {
                 tail = nullptr;
-            }else{
+            } else {
                 head->previous = nullptr;
             }
             cur_size--;
-        }else{
+        } else {
             Node* current = GetNode(pos);
-            if(current->next){
-                current->next->previous=current->previous;
-            }else{
+            if (current->next) {
+                current->next->previous = current->previous;
+            } else {
                 tail = current->previous;
             }
-            current->previous->next=std::move(current->next);
+            current->previous->next = std::move(current->next);
             cur_size--;
         }
     }
@@ -145,31 +145,31 @@ class ListDoublyLinked {
     // TODO: Q 2.6 Discuss the different scenarios for item insertion.
     // Based on the discussion above, implement the Insert method.
     // Insert @item at position @pos
-    void Insert(const T &item, const unsigned int pos){
-        if(pos > cur_size){
+    void Insert(const T &item, const unsigned int pos) {
+        if (pos > cur_size) {
             throw std::out_of_range("Position out of range!");
         }
 
         auto new_node = std::make_unique<Node>();
         new_node->item = item;
 
-        if(pos == 0){
+        if (pos == 0) {
             // Insert at head
             new_node->next = std::move(head);
             new_node->previous = nullptr;
-            if(new_node->next){
+            if (new_node->next) {
                 new_node->next->previous = new_node.get();
-            }else{
+            } else {
                 tail = new_node.get();
             }
             head = std::move(new_node);
-        }else if(pos == cur_size){
+        } else if (pos == cur_size) {
             // Insert at tail
             new_node->next = nullptr;
             new_node->previous = tail;
             tail->next = std::move(new_node);
             tail = tail->next.get();
-        }else{
+        } else {
             // Insert in middle
             Node* prev_node = GetNode(pos - 1);
             new_node->next = std::move(prev_node->next);
