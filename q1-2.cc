c7c1edfed1426676086733c0304f6b73651335a6
ChestnutLUO <digby@65mail.cn>
2026-02-01 22:03:40 -0800

    q1-2

--- q1-2.cc
+++ q1-2.cc
@@ -2,34 +2,45 @@
 
 #include "stack_vector.h"
 
-template<typename T>
+template <typename T>
 void DeleteMiddleElement(Stack<T> &s, unsigned int n, unsigned int curr = 0) {
-    // TODO: Implement this function
-    // @s: stack
-    // @n: size of the stack
-    // @curr: current index
+  if (s.empty())
+    return;
 
+  T temp = s.Top();
+  s.Pop();
 
+  if (curr == n - 1 - n / 2) {
+    return;
+  }
+
+  DeleteMiddleElement(s, n, curr + 1);
+
+  s.Push(temp);
+
+  // Answer to Q1.2:
+  // time Complexity: O(n)
+  // space Complexity: O(n)
 }
 
-#ifndef NO_MAIN  // Only compile main if NO_MAIN is not defined
+#ifndef NO_MAIN // Only compile main if NO_MAIN is not defined
 int main() {
-    Stack<int> s1;
+  Stack<int> s1;
 
-    std::vector<int> v{59, 24, 49, 63, 41};
-    for (auto i : v)
-        s1.Push(i);
+  std::vector<int> v{59, 24, 49, 63, 41};
+  for (auto i : v)
+    s1.Push(i);
 
-    // remove middle item (49)
-    DeleteMiddleElement(s1, s1.Size());
+  // remove middle item (49)
+  DeleteMiddleElement(s1, s1.Size());
 
-    // print s1
-    while (s1.Size() != 0) {
-        std::cout << s1.Top() << ", ";
-        s1.Pop();
-    }
-    std::cout << std::endl;
+  // print s1
+  while (s1.Size() != 0) {
+    std::cout << s1.Top() << ", ";
+    s1.Pop();
+  }
+  std::cout << std::endl;
 
-    return 0;
+  return 0;
 }
 #endif
