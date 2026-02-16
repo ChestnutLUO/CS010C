b3cae767d6eaf5aa5edaa0ea33f7fe83877c04d4
ChestnutLUO <digby@65mail.cn>
2026-02-15 21:55:34 -0800

    Trie tree

--- trie.h
+++ trie.h
@@ -5,45 +5,97 @@
 #include <string>
 
 class TrieNode {
-    public:
-        TrieNode();  // Constructor to initialize members
-        TrieNode* Get(const char c);
-        void Put(const char c, TrieNode *n);
-        bool ContainsKey(const char c);
-        void SetEnd();
-        bool IsEnd();
+public:
+  TrieNode();
+  ~TrieNode();
+  TrieNode *Get(const char c);
+  void Put(const char c, TrieNode *n);
+  bool ContainsKey(const char c);
+  void SetEnd();
+  bool IsEnd();
 
-    private:
-        // TODO: Add private members
-        // Hint: You need to store children nodes and track if this is the end of a word
-
-        // TODO: Add helper function(s)
-        // Hint: Create a function that transforms an ASCII character into an integer 0-25
+private:
+  std::array<TrieNode *, 26> _data{};
+  bool is_end;
+  size_t GetIndex(char c) const { return c - 'a'; }
 };
 
-// TODO: Implementations of TrieNode methods
+TrieNode::TrieNode() : is_end(false) {
+  for (size_t i = 0; i < 26; i++) {
+    _data[i] = nullptr;
+  }
+}
 
+TrieNode::~TrieNode() {
+  for (TrieNode *child : _data) {
+    delete child;
+  }
+}
+
+TrieNode *TrieNode::Get(const char c) { return _data[GetIndex(c)]; }
+
+void TrieNode::Put(const char c, TrieNode *n) { _data[GetIndex(c)] = n; }
+
+bool TrieNode::ContainsKey(const char c) {
+  return _data[GetIndex(c)] != nullptr;
+}
+
+void TrieNode::SetEnd() { is_end = true; }
+
+bool TrieNode::IsEnd() { return is_end; }
 
 class Trie {
-    public:
-        // Inserts @word to the trie
-        void Insert(const std::string &word);
-        // Returns whether @word is found in the trie
-        bool Search(const std::string &word);
-        // Returns whether the trie contains a word that begins with @prefix
-        bool StartsWith(const std::string &prefix);
-    private:
-        // TODO: Add root node
+public:
+  Trie();
+  ~Trie();
+  void Insert(const std::string &word);
+  bool Search(const std::string &word);
+  bool StartsWith(const std::string &prefix);
 
-        // TODO: Add helper function to search against a certain prefix
-        // Hint: This should be used by both Search() and StartsWith()
+private:
+  TrieNode *_root;
+
+  TrieNode *SearchPrefix(const std::string &prefix);
 };
 
-// TODO: Implementations of Trie methods
+Trie::Trie() : _root(new TrieNode()) {}
 
+Trie::~Trie() { delete _root; }
 
-// Q4.1 Trie Usage
+void Trie::Insert(const std::string &word) {
+  TrieNode *node = _root;
+  for (char c : word) {
+    if (!node->ContainsKey(c)) {
+      node->Put(c, new TrieNode());
+    }
+    node = node->Get(c);
+  }
+  node->SetEnd();
+}
+
+TrieNode *Trie::SearchPrefix(const std::string &prefix) {
+  TrieNode *node = _root;
+  for (char c : prefix) {
+    if (!node->ContainsKey(c)) {
+      return nullptr;
+    }
+    node = node->Get(c);
+  }
+  return node;
+}
+
+bool Trie::Search(const std::string &word) {
+  TrieNode *node = SearchPrefix(word);
+  return node != nullptr && node->IsEnd();
+}
+
+bool Trie::StartsWith(const std::string &prefix) {
+  return SearchPrefix(prefix) != nullptr;
+}
+
 // TODO: Answer the question about Trie usage for other languages
-// Your answer: 
+// Your answer: It will effect the length of the subnoes index and deep in tree.
+// Such as Hindi have longer alphabet will cause it longer. Mongolian long words
+// on average cause trie tree more deep in average;
 
 #endif // TRIE_H_
